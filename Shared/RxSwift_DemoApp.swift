80f273f035901bc13e09239d4cfd75a85278c996
Daniel Pitts <daniel.pitts@qgiv.com>
2021-11-18 14:16:30 -0600

    Initial Commit

---
+++ Shared/RxSwift_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwift_DemoApp.swift
+//  Shared
+//
+//  Created by danielpitts on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RxSwift_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
